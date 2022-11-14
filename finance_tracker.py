e6197a740c6679f9255a015a16568ff431380cb4
Krishan Patel <sc17kdp@leeds.ac.uk>
2022-11-14 16:46:19 +0000

    apply changes for multiple csv formats

--- finance_tracker.py
+++ finance_tracker.py
@@ -23,37 +23,58 @@ class BankTransactions():
         # open the correct sheet and worksheet and add the transactions
         sheet = sevice_account.open("Automated Personal Finances")
         worksheet = sheet.worksheet(months[self.month - 1])
-        print(f"inserting rows into {worksheet}...")
+        print(f"inserting rows into {months[self.month - 1]}...")
         worksheet.insert_rows(transactions, row=7)
 
 
 class HSBCTransactions(BankTransactions):
-
-    def get_csv_data(self):
+    def get_transaction_history(self):
         # read in the csv and parse the date in date time format
         temp_df = pd.read_csv(self.csv_path, names=['Date', 'Name', 'Amount'], parse_dates=[0], dayfirst=True)
-
         # only get the month we are looking for
-        df = temp_df[temp_df['Date'].dt.strftime('%m') == str(self.month)]
-
+        df = temp_df[temp_df['Date'].dt.strftime('%m') == str(self.month).zfill(2)]
         # Convert types and add relevant columns
-        df.replace(',', '', regex=True, inplace=True)
+        df = df.replace(',', '', regex=True)
         df = df.astype({'Date': 'str', 'Amount': 'float'})
         df.insert(3, "Bank", "HSBC", allow_duplicates=True)
         df.insert(2, "Category", "Other", allow_duplicates=True)
-        df = self.get_category(df)
+        df = self.get_category(df, 'Name')
 
         # Ensure columns are in the right order
         df = df[['Date', 'Name', 'Category', 'Amount', 'Bank']]
         return df.values.tolist()
 
-    def get_category(self, df):
+    def get_midata_transactions(self):
+        # read in the csv and parse the date in date time format
+        temp_df = pd.read_csv(self.csv_path, parse_dates=[0], dayfirst=True)
+        # only get the month we are looking for
+        df = temp_df[temp_df['Date'].dt.strftime('%m') == str(self.month).zfill(2)]
+        # # Convert types and add relevant columns
+        df = df.replace(',', '', regex=True)
+        df = df.replace('£', '', regex=True)
+
+        df = df.astype({'Date': 'str', 'Debit/Credit': 'float'})
+        df.insert(3, "Bank", "HSBC", allow_duplicates=True)
+        df.insert(2, "Category", "Other", allow_duplicates=True)
+        df = self.get_category(df, 'Merchant/Description')
+
+        # # Ensure columns are in the right order
+        df = df[['Date', 'Merchant/Description', 'Category', 'Debit/Credit', 'Bank']]
+        return df.values.tolist()
+
+    def get_csv_data(self):
+        # transactions = self.get_midata_transactions()
+        transactions = self.get_transaction_history()
+
+        return transactions
+
+    def get_category(self, df, desc_header):
         rent_and_bills_descs = ["VIRGIN MEDIA PYMTS DD", "BARCLAYS UK MTGES DD", "YORKSHIRE WATER DD", "OCTOPUS ENERGY DD"]
         for desc in rent_and_bills_descs:
-            df.loc[df['Name'] == desc, 'Category'] = "Rent and Bills"
+            df.loc[df[desc_header] == desc, 'Category'] = "Rent and Bills"
 
-        df.loc[df['Name'] == "CLUBWISE DD", 'Category'] = "Gym Membership"
-        df.loc[df['Name'] == "BOSCH THERMOTECH CR", 'Category'] = "Salary"
+        df.loc[df[desc_header] == "CLUBWISE DD", 'Category'] = "Gym Membership"
+        df.loc[df[desc_header] == "BOSCH THERMOTECH CR", 'Category'] = "Salary"
         return df
 
 
@@ -62,9 +83,9 @@ class MonzoTransactions(BankTransactions):
         # read in the csv and parse the date in date time format
         temp_df = pd.read_csv(self.csv_path, parse_dates=[1], dayfirst=True)
         # only get the month we are looking for
-        df = temp_df[temp_df['Date'].dt.strftime('%m') == str(self.month)]
-        df['Date'] = df['Date'].astype(str)
-        df['Bank'] = "Monzo"
+        df = temp_df[temp_df['Date'].dt.strftime('%m') == str(self.month).zfill(2)]
+        df = df.astype({'Date': 'str', 'Amount': 'float'})
+        df.insert(3, "Bank", "Monzo", allow_duplicates=True)
         df = df[['Date', 'Name', 'Category', 'Amount', 'Bank']]
         return df.values.tolist()
 
@@ -75,9 +96,10 @@ class AmexTransactions(BankTransactions):
 
 
 def main():
-    hsbc = HSBCTransactions(csv_path='TransactionHistory.csv', month=11)
+    month = 11
+    hsbc = HSBCTransactions(csv_path='TransactionHistory.csv', month=month)
     hsbc.upload()
-    monzo = MonzoTransactions(csv_path='Monzo_November.csv', month=11)
+    monzo = MonzoTransactions(csv_path='MonzoDataExport_1Jan-14Nov_2022-11-14_161142.csv', month=month)
     monzo.upload()
 
 
